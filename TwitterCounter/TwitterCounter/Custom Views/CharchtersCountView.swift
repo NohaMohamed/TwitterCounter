adfde4c1dc796bf8c944e81541d6b6a8924765f8
Noha Mohamed <nohamohamedali1993@gmail.com>
2022-08-14 02:15:35 +0200

    modify characters view count

--- TwitterCounter/TwitterCounter/Custom Views/CharchtersCountView.swift
+++ TwitterCounter/TwitterCounter/Custom Views/CharchtersCountView.swift
@@ -7,11 +7,33 @@
 
 import UIKit
 
-class CharchtersCountView: UIView {
+enum CharchtersCountViewType{
+    case typed
+    case remaining
+}
+
+class CharchtersCountView: UIView ,NibLoadable{
     //MARK: Outlets
     @IBOutlet weak var viewTitleLabel: UILabel!
-    @IBOutlet weak var charchtersTypedLabel: UILabel!
     @IBOutlet weak var slashLabel: UILabel!
+    @IBOutlet weak var charchtersCountLabel: UILabel!
+    @IBOutlet weak var totalCount: UILabel!
     
-    var viewType: CharchtersCountViewType?
+    var viewType: CharchtersCountViewType?{
+        didSet{
+            switch viewType {
+                case .remaining:
+                    slashLabel.text = ""
+                    totalCount.text = ""
+            default:break
+            }
+        }
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        loadNibContent()
+    }
+    func setText(_ str: Int) {
+        charchtersCountLabel.text = "\(str)"
+    }
 }
